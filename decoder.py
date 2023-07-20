f779f8a0696d85fefba904901fc467d65a232f86
ddcvb1 <22andersenmb@gmail.com>
2023-07-19 20:41:13 -0400

    comment

---
+++ decoder.py
@@ -0,0 +1,7 @@
+class Decoder:
+    def decode(password):
+        string_pass = str(password)
+        decoded_password = ""
+        for i in string_pass:
+            decoded_password = decoded_password + str(int(i) - 3)
+        return decoded_passwordgitg
